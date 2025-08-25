f0dc102dcc6f51373c6a2c3c622a7077d97d79d3
Divyanshu Agarwal <divyanshu2102002agarwal@gmail.com>
2025-08-25 20:26:20 +0530

    postgres

--- audiotracker/settings.py
+++ audiotracker/settings.py
@@ -80,13 +80,19 @@ WSGI_APPLICATION = 'audiotracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,   # keeps DB connections open
+        ssl_require=True    # Render requires SSL for Postgres
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
